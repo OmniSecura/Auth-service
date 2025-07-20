f092f58b675e3d3a931296fbf2820a4dbcb49d12
Bartek-Mal <bartekgold8@gmail.com>
2025-07-20 23:12:25 +0200

    db_connection_development: secure.py adjustments, login logic

--- src/routers/v1/auth_router.py
+++ src/routers/v1/auth_router.py
@@ -4,24 +4,36 @@ from sqlalchemy.orm import Session
 
 from src.schemas.RegisterSchema import RegisterSchema
 from src.security.exceptions import user_policies
-from src.database.models.User import User
 from src.database.db_connection import get_db
-from src.services.AuthService import register_user
+from src.services.AuthService import AuthService
 
 auth_router = APIRouter(prefix="/auth", tags=["Auth"])
+
 
 @cbv(auth_router)
 class AuthorizationRouter:
 
     @auth_router.post("/login")
-    async def login(self, username: str, password: str):
-        return {"message": f"Logged in as {username}"}
+    async def login(
+        self,
+        email: str,
+        password: str,
+        db: Session = Depends(get_db),
+    ):
+        auth_service = AuthService(db)
+
+        try:
+            user = auth_service.authenticate_user(email, password)
+        except HTTPException as e:
+            raise e
+
+        return {"message": f"Hi, {user.name}!"}
 
     @auth_router.post("/register", response_model=RegisterSchema)
     async def register(
-            self,
-            register_data: RegisterSchema,
-            db: Session = Depends(get_db),
+        self,
+        register_data: RegisterSchema,
+        db: Session = Depends(get_db),
     ):
         try:
             user_policies(
@@ -35,7 +47,9 @@ class AuthorizationRouter:
         except ValueError as e:
             raise HTTPException(status_code=400, detail=str(e))
 
-        user = register_user(register_data, db)
+        auth_service = AuthService(db)
+        user = auth_service.register_user(register_data)
+
         return RegisterSchema(
             email=user.email,
             name=user.name,
--- src/security/secure.py
+++ src/security/secure.py
@@ -1,7 +1,11 @@
 from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError, InvalidHashError
 
 def hash_password(password: str) -> str:
     return PasswordHasher().hash(password)
 
 def password_verify(password: str, hashed_password: str) -> bool:
-    return PasswordHasher().verify(password, hashed_password)
+    try:
+        return PasswordHasher().verify(hashed_password, password)
+    except (VerifyMismatchError, InvalidHashError):
+        return False
